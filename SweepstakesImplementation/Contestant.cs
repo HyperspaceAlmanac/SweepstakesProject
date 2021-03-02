17f2711a2878327b0f1eeedb22e1888ea531c0fa
Almanac <ruoyw0@gmail.com>
2021-03-02 12:36:56 -0800

    Started filling in some of the files. Saving snapshot now before I import EmailChecker I wrote for Whiteboard Challenge worksheets a few weeks ago

---
+++ SweepstakesImplementation/Contestant.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesImplementation
+{
+    class Contestant
+    {
+        private string firstName;
+        private string lastName;
+        private string email;
+        private string address;
+        private int registrationNumber;
+
+        public void FillOutInformation()
+        {
+            
+        }
+        private void FillOutFirstName()
+        {
+            Console.WriteLine("Please enter your first name:");
+
+        }
+        private void FillOutLastName()
+        {
+            Console.WriteLine("Please enter your last name:");
+        }
+        private void FillOutEmail()
+        {
+            Console.WriteLine("Please fill out your email address:");
+        }
+        private void FillOutAddress()
+        {
+            Console.WriteLine("Please fill out your address:");
+        }
+        private void FillOutRegistrationNumber()
+        {
+            Console.WriteLine("Please enter your registration number:");
+        }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string Email { get; }
+        public string Address { get; }
+        public int RegistrationNumber { get; }
+    }
+}
